683776b3ddd3cfaea31623fbe4df9ee93c0802a5
hxnadoan <imhanadoan@gmail.com>
2026-05-03 18:53:46 -0700

    finally finished the main layout
    It took way too long to learn grids but then it was smooth sailing. Next is going to be the grid layout for the icons.

--- js/homepage/homepage.js
+++ js/homepage/homepage.js
@@ -1,3 +1,6 @@
+/* =====================================================
+ * theme switch toggle
+ * =====================================================*/
 var topuhead = document.getElementById("topuhead");
 
 // on click function that switches between light/dark modes
@@ -14,3 +17,44 @@ topuhead.onclick = function(){
         topuhead.src = "../../assets/media/angel/topuhead.PNG";
     }
 }
+
+/* =====================================================
+ * scrambled text
+ * =====================================================*/
+var text = document.getElementById("scramble");
+let randomChars = "!@#$%&*()_+-<>";
+let iterations = 0;
+let originalText = text.textContent;
+let interval = null;
+
+function startScramble() {
+    iterations = 0;
+
+    // scrambling interval that retuns random characters
+    // in place of original character index in string
+    interval = setInterval(() => {
+        text.textContent = originalText.split("").map((char, index) => {
+            return randomChars.charAt(Math.floor(Math.random() 
+            * randomChars.length));
+        }).join("");
+
+        if (iterations >= originalText.length) {
+            iterations = 0;
+        } else {
+            iterations += 1/16; // speed
+        }
+    }, 90); // delay
+}
+
+// reveals original string
+function stopScramble(){
+    clearInterval(interval);
+    interval = null;
+    text.textContent = originalText;
+}
+
+// on hover reveals the message
+text.addEventListener("mouseenter", stopScramble);
+text.addEventListener("mouseleave", startScramble);
+
+startScramble();
