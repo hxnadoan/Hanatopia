66bf7c255ebae1945a43a85e840e3dfd32b86245
hxnadoan <imhanadoan@gmail.com>
2026-06-04 17:33:52 -0700

    finished setting up general modal formatting
    Added a new door asset for the About Me page. Also forgot to mention but decided to move changelog to project to make documenting more streamlined.

--- js/homepage/homepage.js
+++ js/homepage/homepage.js
@@ -117,6 +117,41 @@ document.addEventListener("DOMContentLoaded", function() {
 /* =====================================================
  * icon buttons
  * =====================================================*/
+
+// --- MODALS ---
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-butt]');
+const overlay = document.getElementById('overlay');
+
+function openModal(modal) {
+    if(modal == null) 
+        return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if(modal == null) 
+        return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+// --- GITHUB ---
 const githubLink = document.getElementById("github-img");
 
 githubLink.addEventListener('click', () => {
