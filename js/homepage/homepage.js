f857b5136736c349a7ab55202ae11b8407840368
hxnadoan <imhanadoan@gmail.com>
2026-06-30 14:49:50 -0700

    added local storage memory for theme
    glorp and angel themes should remain regardless of page switches or if you reload the page

--- js/homepage/homepage.js
+++ js/homepage/homepage.js
@@ -9,12 +9,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // (media queries hiding them was giving issues with 
     // the direct var-> id -> src swaps)
     function swapSrc(id, src) {
-    var el = document.getElementById(id);
-    if (el) el.src = src;
+        var el = document.getElementById(id);
+        if (el) el.src = src;
     }
 
-    // center icons var
+    // theme toggle button (img)
     var topuhead = document.getElementById("topuhead");
+
+    // center icons var
     var aboutme = document.getElementById("about-me-img");
     var github = document.getElementById("github-img");
     var edu = document.getElementById("education-img");
@@ -30,89 +32,100 @@ document.addEventListener("DOMContentLoaded", function() {
     var miniham = document.getElementById("minihana");
     var hana = document.getElementById("hana");
 
-if (topuhead) {
-        // on click function that switches between light/dark modes
-        topuhead.onclick = function(){
-            document.documentElement.classList.toggle("glorp-mode");
+    // theme switch key
+    var STORAGE_KEY = "glorp-mode";
 
-            // replaces assets based on light/dark modes
-            if(document.documentElement.classList.contains("glorp-mode"))
-            {            
-                swapSrc("nav-home", "../../assets/media/glorp/glorpuhead.PNG");
-                swapSrc("nav-abtme", "../../assets/media/glorp/icons/angelglorp.png");
-                swapSrc("nav-git", "../../assets/media/glorp/icons/glorp1.png");
-                swapSrc("nav-edu", "../../assets/media/glorp/icons/shootingstar.png");
-                swapSrc("nav-exp", "../../assets/media/glorp/icons/glorpship.png");
-                swapSrc("nav-skills", "../../assets/media/glorp/icons/moon.png");
-                swapSrc("nav-chnglg", "../../assets/media/glorp/icons/glorpnotepad.png");
-                swapSrc("nav-build", "../../assets/media/glorp/icons/shrinkray.png");
-                swapSrc("nav-gb", "../../assets/media/glorp/icons/cownap.png");
+    function enableGlorp(isGlorp) {
+        document.documentElement.classList.toggle("glorp-mode", isGlorp);
 
-                swapSrc("burg-home", "../../assets/media/glorp/glorpuhead.PNG");
-                swapSrc("burg-abtme", "../../assets/media/glorp/icons/angelglorp.png");
-                swapSrc("burg-git", "../../assets/media/glorp/icons/glorp1.png");
-                swapSrc("burg-edu", "../../assets/media/glorp/icons/shootingstar.png");
-                swapSrc("burg-exp", "../../assets/media/glorp/icons/glorpship.png");
-                swapSrc("burg-skills", "../../assets/media/glorp/icons/moon.png");
-                swapSrc("burg-chnglg", "../../assets/media/glorp/icons/glorpnotepad.png");
-                swapSrc("burg-build", "../../assets/media/glorp/icons/shrinkray.png");
-                swapSrc("burg-gb", "../../assets/media/glorp/icons/cownap.png");
+        // replaces assets based on light/dark modes
+        if(isGlorp) {            
+            swapSrc("nav-home", "../../assets/media/glorp/glorpuhead.PNG");
+            swapSrc("nav-abtme", "../../assets/media/glorp/icons/angelglorp.png");
+            swapSrc("nav-git", "../../assets/media/glorp/icons/glorp1.png");
+            swapSrc("nav-edu", "../../assets/media/glorp/icons/shootingstar.png");
+            swapSrc("nav-exp", "../../assets/media/glorp/icons/glorpship.png");
+            swapSrc("nav-skills", "../../assets/media/glorp/icons/moon.png");
+            swapSrc("nav-chnglg", "../../assets/media/glorp/icons/glorpnotepad.png");
+            swapSrc("nav-build", "../../assets/media/glorp/icons/shrinkray.png");
+            swapSrc("nav-gb", "../../assets/media/glorp/icons/cownap.png");
 
-                if (topuhead) topuhead.src = "../../assets/media/glorp/glorpuhead.PNG";
-                if (aboutme) aboutme.src = "../../assets/media/glorp/icons/angelglorp.png";
-                if (github) github.src = "../../assets/media/glorp/icons/glorp1.png";
-                if (edu) edu.src = "../../assets/media/glorp/icons/shootingstar.png";
-                if (exp) exp.src = "../../assets/media/glorp/icons/glorpship.png";
-                if (skills) skills.src = "../../assets/media/glorp/icons/moon.png";
-                if (changelog) changelog.src = "../../assets/media/glorp/icons/glorpnotepad.png";
-                if (sitebuild) sitebuild.src = "../../assets/media/glorp/icons/shrinkray.png";
-                if (guestbook) guestbook.src = "../../assets/media/glorp/icons/cownap.png";
+            swapSrc("burg-home", "../../assets/media/glorp/glorpuhead.PNG");
+            swapSrc("burg-abtme", "../../assets/media/glorp/icons/angelglorp.png");
+            swapSrc("burg-git", "../../assets/media/glorp/icons/glorp1.png");
+            swapSrc("burg-edu", "../../assets/media/glorp/icons/shootingstar.png");
+            swapSrc("burg-exp", "../../assets/media/glorp/icons/glorpship.png");
+            swapSrc("burg-skills", "../../assets/media/glorp/icons/moon.png");
+            swapSrc("burg-chnglg", "../../assets/media/glorp/icons/glorpnotepad.png");
+            swapSrc("burg-build", "../../assets/media/glorp/icons/shrinkray.png");
+            swapSrc("burg-gb", "../../assets/media/glorp/icons/cownap.png");
 
-                if (frame) frame.src = "../../assets/media/glorp/centerframeglorp.png";
-                if (dialoguebox) dialoguebox.src = "../../assets/media/glorp/dialogueboxglorp.png";
-                if (miniham) miniham.src = "../../assets/media/glorp/hanaminigreen.png";
+            if (topuhead) topuhead.src = "../../assets/media/glorp/glorpuhead.PNG";
+            if (aboutme) aboutme.src = "../../assets/media/glorp/icons/angelglorp.png";
+            if (github) github.src = "../../assets/media/glorp/icons/glorp1.png";
+            if (edu) edu.src = "../../assets/media/glorp/icons/shootingstar.png";
+            if (exp) exp.src = "../../assets/media/glorp/icons/glorpship.png";
+            if (skills) skills.src = "../../assets/media/glorp/icons/moon.png";
+            if (changelog) changelog.src = "../../assets/media/glorp/icons/glorpnotepad.png";
+            if (sitebuild) sitebuild.src = "../../assets/media/glorp/icons/shrinkray.png";
+            if (guestbook) guestbook.src = "../../assets/media/glorp/icons/cownap.png";
 
-                if (hana) hana.src = "../../assets/media/glorp/hanagreenp.png";
-            }
-            else
-            {
-                swapSrc("nav-home", "../../assets/media/angel/topuhead.PNG");
-                swapSrc("nav-abtme", "../../assets/media/angel/icons/heartwing.png");
-                swapSrc("nav-git", "../../assets/media/angel/icons/gemheart1.png");
-                swapSrc("nav-edu", "../../assets/media/angel/icons/starhalo.png");
-                swapSrc("nav-exp", "../../assets/media/angel/icons/heartcloud.png");
-                swapSrc("nav-skills", "../../assets/media/angel/icons/sun.png");
-                swapSrc("nav-chnglg", "../../assets/media/angel/icons/topunotepad.png");
-                swapSrc("nav-build", "../../assets/media/angel/icons/magicwand.png");
-                swapSrc("nav-gb", "../../assets/media/angel/icons/tofuangel.png");
+            if (frame) frame.src = "../../assets/media/glorp/centerframeglorp.png";
+            if (dialoguebox) dialoguebox.src = "../../assets/media/glorp/dialogueboxglorp.png";
+            if (miniham) miniham.src = "../../assets/media/glorp/hanaminigreen.png";
 
-                swapSrc("burg-home", "../../assets/media/angel/topuhead.PNG");
-                swapSrc("burg-abtme", "../../assets/media/angel/icons/heartwing.png");
-                swapSrc("burg-git", "../../assets/media/angel/icons/gemheart1.png");
-                swapSrc("burg-edu", "../../assets/media/angel/icons/starhalo.png");
-                swapSrc("burg-exp", "../../assets/media/angel/icons/heartcloud.png");
-                swapSrc("burg-skills", "../../assets/media/angel/icons/sun.png");
-                swapSrc("burg-chnglg", "../../assets/media/angel/icons/topunotepad.png");
-                swapSrc("burg-build", "../../assets/media/angel/icons/magicwand.png");
-                swapSrc("burg-gb", "../../assets/media/angel/icons/tofuangel.png");
+            if (hana) hana.src = "../../assets/media/glorp/hanagreenp.png";
+        } else {
+            swapSrc("nav-home", "../../assets/media/angel/topuhead.PNG");
+            swapSrc("nav-abtme", "../../assets/media/angel/icons/heartwing.png");
+            swapSrc("nav-git", "../../assets/media/angel/icons/gemheart1.png");
+            swapSrc("nav-edu", "../../assets/media/angel/icons/starhalo.png");
+            swapSrc("nav-exp", "../../assets/media/angel/icons/heartcloud.png");
+            swapSrc("nav-skills", "../../assets/media/angel/icons/sun.png");
+            swapSrc("nav-chnglg", "../../assets/media/angel/icons/topunotepad.png");
+            swapSrc("nav-build", "../../assets/media/angel/icons/magicwand.png");
+            swapSrc("nav-gb", "../../assets/media/angel/icons/tofuangel.png");
 
-                if (topuhead) topuhead.src = "../../assets/media/angel/topuhead.PNG";
-                if (aboutme) aboutme.src = "../../assets/media/angel/icons/heartwing.png";
-                if (github) github.src = "../../assets/media/angel/icons/gemheart1.png";
-                if (edu) edu.src = "../../assets/media/angel/icons/starhalo.png";
-                if (exp) exp.src = "../../assets/media/angel/icons/heartcloud.png";
-                if (skills) skills.src = "../../assets/media/angel/icons/sun.png";
-                if (changelog) changelog.src = "../../assets/media/angel/icons/topunotepad.png";
-                if (sitebuild) sitebuild.src = "../../assets/media/angel/icons/magicwand.png";
-                if (guestbook) guestbook.src = "../../assets/media/angel/icons/tofuangel.png";
+            swapSrc("burg-home", "../../assets/media/angel/topuhead.PNG");
+            swapSrc("burg-abtme", "../../assets/media/angel/icons/heartwing.png");
+            swapSrc("burg-git", "../../assets/media/angel/icons/gemheart1.png");
+            swapSrc("burg-edu", "../../assets/media/angel/icons/starhalo.png");
+            swapSrc("burg-exp", "../../assets/media/angel/icons/heartcloud.png");
+            swapSrc("burg-skills", "../../assets/media/angel/icons/sun.png");
+            swapSrc("burg-chnglg", "../../assets/media/angel/icons/topunotepad.png");
+            swapSrc("burg-build", "../../assets/media/angel/icons/magicwand.png");
+            swapSrc("burg-gb", "../../assets/media/angel/icons/tofuangel.png");
 
-                if (frame) frame.src = "../../assets/media/angel/centerframe.png";
-                if (dialoguebox) dialoguebox.src = "../../assets/media/angel/dialoguebox.png";
-                if (miniham) miniham.src = "../../assets/media/angel/hanaminipink.png";
+            if (topuhead) topuhead.src = "../../assets/media/angel/topuhead.PNG";
+            if (aboutme) aboutme.src = "../../assets/media/angel/icons/heartwing.png";
+            if (github) github.src = "../../assets/media/angel/icons/gemheart1.png";
+            if (edu) edu.src = "../../assets/media/angel/icons/starhalo.png";
+            if (exp) exp.src = "../../assets/media/angel/icons/heartcloud.png";
+            if (skills) skills.src = "../../assets/media/angel/icons/sun.png";
+            if (changelog) changelog.src = "../../assets/media/angel/icons/topunotepad.png";
+            if (sitebuild) sitebuild.src = "../../assets/media/angel/icons/magicwand.png";
+            if (guestbook) guestbook.src = "../../assets/media/angel/icons/tofuangel.png";
 
-                if (hana) hana.src = "../../assets/media/angel/hanapinkp.png";
-            }
+            if (frame) frame.src = "../../assets/media/angel/centerframe.png";
+            if (dialoguebox) dialoguebox.src = "../../assets/media/angel/dialoguebox.png";
+            if (miniham) miniham.src = "../../assets/media/angel/hanaminipink.png";
+
+            if (hana) hana.src = "../../assets/media/angel/hanapinkp.png";
         }
+    }
+
+// restore saved theme on load
+    var saved = localStorage.getItem(STORAGE_KEY);
+    if (saved === "active") {
+        enableGlorp(true);
+    }
+
+    if (topuhead) {
+        topuhead.onclick = function () {
+            var goingGlorp = !document.documentElement.classList.contains("glorp-mode");
+            enableGlorp(goingGlorp);
+            localStorage.setItem(STORAGE_KEY, goingGlorp ? "active" : "inactive");
+        };
     }
 });
 
